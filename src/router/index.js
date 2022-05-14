36f3d6147f9ee4b3b9fe94d5614ce700a327e4ab
Marcellinus <ignacymischa@gmail.com>
2022-05-14 16:40:41 +0700

    update fitur

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import ProductView from '../views/ProductView.vue'
+import ShoppingCart from "../views/ShoppingCartView.vue"
+import SuksesView from "../views/SuksesView.vue"
 
 Vue.use(VueRouter)
 
@@ -17,7 +20,22 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  },
+  {
+    path: '/product',
+    name: 'product',
+    component: ProductView
+  },
+  {
+    path: '/cart',
+    name: 'ShoppingCart',
+    component: ShoppingCart
+  },
+  {
+    path: '/sukses',
+    name: 'sukses',
+    component: SuksesView
+  },
 ]
 
 const router = new VueRouter({
