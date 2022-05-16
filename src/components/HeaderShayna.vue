459a58d0a1698570dff286b6d0376ef8bf85451e
Marcellinus <ignacymischa@gmail.com>
2022-05-16 17:23:55 +0700

    update fitur dan bug

--- src/components/HeaderShayna.vue
+++ src/components/HeaderShayna.vue
@@ -30,49 +30,42 @@
                                     Keranjang Belanja &nbsp;
                                     <a href="#">
                                         <i class="icon_bag_alt"></i>
-                                        <span>3</span>
+                                        <span>{{keranjangUser.length}}</span>
                                     </a>
                                     <div class="cart-hover">
                                         <div class="select-items">
                                             <table>
-                                                <tbody>
-                                                    <tr>
+                                                <tbody v-if="keranjangUser.length > 0">
+                                                    <tr v-for="keranjang in keranjangUser" :key="keranjang.id">
                                                         <td class="si-pic">
-                                                            <img src="img/select-product-1.jpg" alt="" />
+                                                            <img class="photo-item" :src="keranjang.photo" alt="" />
                                                         </td>
                                                         <td class="si-text">
                                                             <div class="product-selected">
-                                                                <p>$60.00 x 1</p>
-                                                                <h6>Kabino Bedside Table</h6>
+                                                                <p>${{keranjang.price}} x 1</p>
+                                                                <h6>{{keranjang.name}}</h6>
                                                             </div>
                                                         </td>
-                                                        <td class="si-close">
+                                                        <td @click="removeItem(keranjangUser.index)" class="si-close">
                                                             <i class="ti-close"></i>
                                                         </td>
                                                     </tr>
+
+                                                </tbody>
+                                                <tbody v-else>
                                                     <tr>
-                                                        <td class="si-pic">
-                                                            <img src="img/select-product-2.jpg" alt="" />
-                                                        </td>
-                                                        <td class="si-text">
-                                                            <div class="product-selected">
-                                                                <p>$60.00 x 1</p>
-                                                                <h6>Kabino Bedside Table</h6>
-                                                            </div>
-                                                        </td>
-                                                        <td class="si-close">
-                                                            <i class="ti-close"></i>
-                                                        </td>
+                                                        <td> Keranjang Kosong</td>
                                                     </tr>
                                                 </tbody>
                                             </table>
                                         </div>
                                         <div class="select-total">
                                             <span>total:</span>
-                                            <h5>$120.00</h5>
+                                            <h5>${{totalHarga}}.00</h5>
                                         </div>
+
                                         <div class="select-button">
-                                            <a href="#" class="primary-btn view-card">VIEW CARD</a>
+                                            <a href="#" class="primary-btn view-card"><router-link to="/cart" style="color: #FFF;">VIEW CARD</router-link></a>
                                             <a href="#" class="primary-btn checkout-btn">CHECK OUT</a>
                                         </div>
                                     </div>
@@ -88,6 +81,49 @@
 
 <script>
 export default {
-    name:'HeaderShayna'
+    name:'HeaderShayna',
+    data() {
+        return {
+            keranjangUser: []
+        };
+    },
+    methods:{
+        removeItem(idx){
+            // cari tahu id dari si item yang akan dihapus
+            let keranjangUserStorage = JSON.parse(localStorage.getItem("keranjangUser"));
+            let itemKeranjangUserStorage = keranjangUserStorage.map(itemKeranjangUserStorage => itemKeranjangUserStorage.id);
+
+            // cocokan idx item dengan id yang ada di storage
+            let index = itemKeranjangUserStorage.findIndex(id => id == idx);
+            this.keranjangUser.splice(index, 1);
+
+            const parsed = JSON.stringify(this.keranjangUser);
+            localStorage.setItem("keranjangUser", parsed);
+            window.location.reload();
+        }
+    },
+    mounted() {
+        if (localStorage.getItem('keranjangUser')) {
+        try {
+            this.keranjangUser = JSON.parse(localStorage.getItem('keranjangUser'));
+        } catch(e) {
+            localStorage.removeItem('keranjang.id');
+        }
+        }
+    },
+    computed: {
+        totalHarga(){
+            return this.keranjangUser.reduce(function (items, data) {
+                return items + data.price;
+            }, 0);
+        }
+    }
 }
 </script>
+
+<style scoped>
+.photo-item{
+    height: 80px;
+    width: 80px;
+}
+</style>
--- src/views/ShoppingCartView.vue
+++ src/views/ShoppingCartView.vue
@@ -7,7 +7,7 @@
                 <div class="row">
                     <div class="col-lg-12">
                         <div class="breadcrumb-text product-more text-left">
-                            <router-link to="/sukses">
+                            <router-link to="/">
                             <i class="fa fa-home">
                             </i> Home
                             </router-link>
@@ -36,30 +36,20 @@
                                             </tr>
                                         </thead>
                                         <tbody>
-                                            <tr>
+
+                                            <tr v-for="keranjang in keranjangUser" :key="keranjang.id">
                                                 <td class="cart-pic first-row">
-                                                    <img src="img/cart-page/product-1.jpg" />
+                                                    <img :src="keranjang.photo" />
                                                 </td>
                                                 <td class="cart-title first-row text-center">
-                                                    <h5>Pure Pineapple</h5>
+                                                    <h5>{{keranjang.name}}</h5>
                                                 </td>
-                                                <td class="p-price first-row">$60.00</td>
-                                                <td class="delete-item"><a href="#"><i class="material-icons">
+                                                <td class="p-price first-row">${{keranjang.price}}</td>
+                                                <td @click="removeItem(keranjangUser.index)" class="delete-item"><a href="#"><i class="material-icons">
                                                 close
                                                 </i></a></td>
                                             </tr>
-                                            <tr>
-                                                <td class="cart-pic first-row">
-                                                    <img src="img/cart-page/product-1.jpg" />
-                                                </td>
-                                                <td class="cart-title first-row text-center">
-                                                    <h5>Pure Pineapple</h5>
-                                                </td>
-                                                <td class="p-price first-row">$60.00</td>
-                                                <td class="delete-item"><a href="#"><i class="material-icons">
-                                                close
-                                                </i></a></td>
-                                            </tr>
+                                            
                                         </tbody>
                                     </table>
                                 </div>
@@ -72,19 +62,19 @@
                                     <form>
                                         <div class="form-group">
                                             <label for="namaLengkap">Nama lengkap</label>
-                                            <input type="text" class="form-control" id="namaLengkap" aria-describedby="namaHelp" placeholder="Masukan Nama">
+                                            <input type="text" class="form-control" id="namaLengkap" aria-describedby="namaHelp" placeholder="Masukan Nama" v-model="customerInfo.name">
                                         </div>
                                         <div class="form-group">
                                             <label for="namaLengkap">Email Address</label>
-                                            <input type="email" class="form-control" id="emailAddress" aria-describedby="emailHelp" placeholder="Masukan Email">
+                                            <input type="email" class="form-control" id="emailAddress" aria-describedby="emailHelp" placeholder="Masukan Email" v-model="customerInfo.email">
                                         </div>
                                         <div class="form-group">
                                             <label for="namaLengkap">No. HP</label>
-                                            <input type="text" class="form-control" id="noHP" aria-describedby="noHPHelp" placeholder="Masukan No. HP">
+                                            <input type="text" class="form-control" id="noHP" aria-describedby="noHPHelp" placeholder="Masukan No. HP" v-model="customerInfo.number">
                                         </div>
                                         <div class="form-group">
                                             <label for="alamatLengkap">Alamat Lengkap</label>
-                                            <textarea class="form-control" id="alamatLengkap" rows="3"></textarea>
+                                            <textarea class="form-control" id="alamatLengkap" rows="3" v-model="customerInfo.address"></textarea>
                                         </div>
                                     </form>
                                 </div>
@@ -97,15 +87,16 @@
                                 <div class="proceed-checkout text-left">
                                     <ul>
                                         <li class="subtotal">ID Transaction <span>#SH12000</span></li>
-                                        <li class="subtotal mt-3">Subtotal <span>$240.00</span></li>
+                                        <li class="subtotal mt-3">Subtotal <span>${{totalHarga}}.00</span></li>
                                         <li class="subtotal mt-3">Pajak <span>10%</span></li>
-                                        <li class="subtotal mt-3">Total Biaya <span>$440.00</span></li>
+                                        <li class="subtotal mt-3">Total Pajak<span>{{ditambahPajak}}</span></li>
+                                        <li class="subtotal mt-3">Total Biaya <span>${{totalBiaya}}.00</span></li>
                                         <li class="subtotal mt-3">Bank Transfer <span>Mandiri</span></li>
                                         <li class="subtotal mt-3">No. Rekening <span>2208 1996 1403</span></li>
                                         <li class="subtotal mt-3">Nama Penerima <span>Shayna</span></li>
                                     </ul>
                                     <router-link to="/sukses">
-                                    <a href="success.html" class="proceed-btn">I ALREADY PAID</a>
+                                    <a @click="checkout()" href="#" class="proceed-btn">I ALREADY PAID</a>
                                     </router-link>
                                 </div>
                             </div>
@@ -120,11 +111,73 @@
 
 <script>
 import HeaderShayna from '@/components/HeaderShayna.vue'
+import axios from "axios";
 
 export default {
     name: "ShoppingCart",
     components:{
         HeaderShayna
+    },
+    checkout(){
+        let prodcutIds = this.keranjangUser.map(function (product) {
+            return product.id
+        });
+
+        let checkoutData = {
+            'name': this.customerInfo.name,
+            'email': this.customerInfo.email,
+            'number': this.customerInfo.number,
+            'address': this.customerInfo.address,
+            "transaction_total": this.totalBiaya,
+            "transaction_status": "PENDING",
+            "transaction_details": prodcutIds
+        }
+
+        axios
+            .get("https://shayna-backend.buildwithangga.id/api/products", checkoutData)
+            .then(() => this.$router.push('success'))
+            // eslint-disable-next-line no-console
+            .catch((err) => console.log(err));
+    },
+    data() {
+        return {
+            keranjangUser: [],
+            customerInfo: {
+                name: '',
+                email: '',
+                number: '',
+                address: ''
+            }
+        };
+    },
+    methods:{
+        removeItem(index){
+            this.keranjangUser.splice(index,1);
+            const parsed = JSON.stringify(this.keranjangUser);
+            localStorage.setItem('keranjangUser', parsed);
+        }
+    },
+    mounted() {
+        if (localStorage.getItem('keranjangUser')) {
+            try {
+                this.keranjangUser = JSON.parse(localStorage.getItem('keranjangUser'));
+            } catch(e) {
+                localStorage.removeItem('keranjangUser');
+            }
+        }
+    },
+    computed: {
+        totalHarga(){
+            return this.keranjangUser.reduce(function (items, data) {
+                return items + data.price;
+            }, 0);
+        },
+        ditambahPajak() {
+            return (this.totalHarga * 10) /100;
+        },
+        totalBiaya(){
+            return this.totalHarga + this.ditambahPajak;
+        }
     }
 }
 </script>
